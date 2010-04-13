c72d9ae8fe874da1dc1eb27a6c2c67e0dcac2747
Frank Richter <frank.richter@gmail.com>
2010-04-13 16:04:39 +0200

    Some more precision tests

--- lib/parser/t/ParserCommonTypeCompat.cpp
+++ lib/parser/t/ParserCommonTypeCompat.cpp
@@ -23,6 +23,10 @@ class ParserCommonTypeCompatTestSuite : public CxxTest::TestSuite
   
   class TestSemanticsHandler : public TestSemanticsHandlerTemplated<0>
   {
+  public:
+    typedef TestSemanticsHandlerTemplated<0> SuperClass;
+    
+    using SuperClass::GetHigherPrecisionType;
   };
 public:
   void testCompatBase (void)
@@ -80,6 +84,7 @@ public:
       
       TS_ASSERT_EQUALS (typeInt->CompatibleLossless (*typeFloat), true);
       TS_ASSERT_EQUALS (typeInt->CompatibleLossy (*typeFloat), true);
+      // int is lower prec than float
       TS_ASSERT_EQUALS (typeInt->IsPrecisionHigherEqual (*typeFloat), false);
     }
     // UInt - compatible to other numeric types
@@ -90,6 +95,7 @@ public:
       
       TS_ASSERT_EQUALS (typeUInt->CompatibleLossless (*typeInt), true);
       TS_ASSERT_EQUALS (typeUInt->CompatibleLossy (*typeInt), true);
+      // uint, despite "lossless" assignable to, is lower prec than int!
       TS_ASSERT_EQUALS (typeUInt->IsPrecisionHigherEqual (*typeInt), false);
       
       TS_ASSERT_EQUALS (typeUInt->CompatibleLossless (*typeUInt), true);
@@ -98,6 +104,7 @@ public:
       
       TS_ASSERT_EQUALS (typeUInt->CompatibleLossless (*typeFloat), true);
       TS_ASSERT_EQUALS (typeUInt->CompatibleLossy (*typeFloat), true);
+      // uint is lower prec than float
       TS_ASSERT_EQUALS (typeUInt->IsPrecisionHigherEqual (*typeFloat), false);
     }
     // Float - compatible to other numeric types
@@ -106,17 +113,58 @@ public:
       TS_ASSERT_EQUALS (typeFloat->CompatibleLossy (*typeBool), false);
       TS_ASSERT_EQUALS (typeFloat->IsPrecisionHigherEqual (*typeBool), false);
       
+      // float -> int not lossless
       TS_ASSERT_EQUALS (typeFloat->CompatibleLossless (*typeInt), false);
       TS_ASSERT_EQUALS (typeFloat->CompatibleLossy (*typeInt), true);
+      // float is higher prec than int
       TS_ASSERT_EQUALS (typeFloat->IsPrecisionHigherEqual (*typeInt), true);
       
+      // float -> uint not lossless
       TS_ASSERT_EQUALS (typeFloat->CompatibleLossless (*typeUInt), false);
       TS_ASSERT_EQUALS (typeFloat->CompatibleLossy (*typeUInt), true);
+      // float is higher prec than uint
       TS_ASSERT_EQUALS (typeFloat->IsPrecisionHigherEqual (*typeUInt), true);
       
       TS_ASSERT_EQUALS (typeFloat->CompatibleLossless (*typeFloat), true);
       TS_ASSERT_EQUALS (typeFloat->CompatibleLossy (*typeFloat), true);
       TS_ASSERT_EQUALS (typeFloat->IsPrecisionHigherEqual (*typeFloat), true);
     }
+  }
+  
+  void testHigherPrecision (void)
+  {
+    using namespace s1::parser;
+    typedef TestSemanticsHandler::TestType TestType;
+    typedef boost::shared_ptr<TestType> TestTypePtr;
+    
+    TestSemanticsHandler semanticsHandler;
+    
+    TestTypePtr typeBool;
+    TS_ASSERT_THROWS_NOTHING ((typeBool = boost::shared_static_cast<TestType> (
+      semanticsHandler.CreateType (CommonSemanticsHandler::Bool))));
+    TestTypePtr typeInt;
+    TS_ASSERT_THROWS_NOTHING ((typeInt = boost::shared_static_cast<TestType> (
+      semanticsHandler.CreateType (CommonSemanticsHandler::Int))));
+    TestTypePtr typeUInt;
+    TS_ASSERT_THROWS_NOTHING ((typeUInt = boost::shared_static_cast<TestType> (
+      semanticsHandler.CreateType (CommonSemanticsHandler::UInt))));
+    TestTypePtr typeFloat;
+    TS_ASSERT_THROWS_NOTHING ((typeFloat = boost::shared_static_cast<TestType> (
+      semanticsHandler.CreateType (CommonSemanticsHandler::Float))));
+    
+    TS_ASSERT_EQUALS (TestSemanticsHandler::GetHigherPrecisionType (typeBool, typeInt),
+		      TestTypePtr ());
+    TS_ASSERT_EQUALS (TestSemanticsHandler::GetHigherPrecisionType (typeBool, typeUInt),
+		      TestTypePtr ());
+    TS_ASSERT_EQUALS (TestSemanticsHandler::GetHigherPrecisionType (typeBool, typeFloat),
+		      TestTypePtr ());
+      
+    TS_ASSERT_EQUALS (TestSemanticsHandler::GetHigherPrecisionType (typeInt, typeUInt),
+		      typeInt);
+    TS_ASSERT_EQUALS (TestSemanticsHandler::GetHigherPrecisionType (typeInt, typeFloat),
+		      typeFloat);
+      
+    TS_ASSERT_EQUALS (TestSemanticsHandler::GetHigherPrecisionType (typeUInt, typeFloat),
+		      typeFloat);
   }
 };
