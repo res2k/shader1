9783f71f4df9f5a153aed0dc294e25089972cfd3
Frank Richter <frank.richter@gmail.com>
2010-05-24 03:39:18 +0200

    Emit proper function declarations

--- lib/codegen/FunctionCodeGenerator.cpp
+++ lib/codegen/FunctionCodeGenerator.cpp
@@ -14,8 +14,36 @@ namespace s1
   {
     StringsArrayPtr CgGenerator::FunctionCodeGenerator::Generate (const intermediate::ProgramFunctionPtr& func)
     {
+      typedef parser::SemanticsHandler::Scope::FunctionFormalParameters FunctionFormalParameters;
+      
       StringsArrayPtr resultStrings (boost::make_shared<StringsArray> ());
-      resultStrings->AddString (std::string ("// TODO: proper function declaration"));
+      {
+	std::string typeStr (TypeToCgType (func->GetReturnType()));
+	std::string identifier (NameToCgIdentifier (func->GetIdentifier()));
+	
+	std::string funcDecl (typeStr);
+	funcDecl.append (" ");
+	funcDecl.append (identifier);
+	funcDecl.append (" (");
+	bool firstParam = true;
+	const FunctionFormalParameters& params (func->GetParams());
+	for (FunctionFormalParameters::const_iterator param = params.begin();
+	     param != params.end();
+	     ++param)
+	{
+	  std::string paramStr;
+	  if (!firstParam)
+	    paramStr = ", ";
+	  else
+	    firstParam = false;
+	  paramStr.append (TypeToCgType (param->type));
+	  paramStr.append (" ");
+	  paramStr.append (NameToCgIdentifier (param->identifier));
+	  funcDecl.append (paramStr);
+	}
+	funcDecl.append (")");
+	resultStrings->AddString (funcDecl);
+      }
       resultStrings->AddString (std::string ("{"));
       
       BlockNameResolver nameRes;
