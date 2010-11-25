a979750ad2c6b32da0d302252e1d02755418d45b
Frank Richter <frank.richter@gmail.com>
2010-11-25 18:06:30 +0100

    More inliner tests

--- lib/optimize/t/InlineBlock.cpp
+++ lib/optimize/t/InlineBlock.cpp
@@ -303,4 +303,64 @@ public:
     TS_ASSERT_EQUALS (testVisitor.entries[0].branchElseVisitor->entries[0].blockVisitor->entries[0].op, TestSequenceVisitor::opConstUInt);
   }
   
+  void testNestedBlockWithCondition (void)
+  {
+    static const char blockSource[] =
+      "unsigned int a;"
+      "bool c;"
+      "{"
+      "  if (c)"
+      "  {"
+      "    a = 1;"
+      "  }"
+      "  else"
+      "  {"
+      "    a = 2;"
+      "  }"
+      "}"
+      ;
+    
+    using namespace s1::parser;
+    
+    std::istringstream in (blockSource);
+    s1::UnicodeStream ustream (in, "utf-8");
+    s1::LexerErrorHandler errorHandler;
+    s1::Lexer lexer (ustream, errorHandler);
+    TestSemanticsHandler semanticsHandler;
+    s1::parser::ErrorHandler parserErrorHandler;
+    TestParser parser (lexer, semanticsHandler, parserErrorHandler);
+    
+    // global scope is required so BlockImpl can create some unique var names
+    SemanticsHandler::ScopePtr globalScope (semanticsHandler.CreateScope (SemanticsHandler::ScopePtr(),
+					    TestSemanticsHandler::Global,
+					    semanticsHandler.GetVoidType()));
+    SemanticsHandler::BlockPtr block (
+      semanticsHandler.CreateBlock (globalScope));
+    TS_ASSERT_THROWS_NOTHING(parser.ParseBlock (block));
+    
+    boost::shared_ptr<TestSemanticsHandler::TestBlockImpl> testBlockImpl (
+      boost::shared_static_cast<TestSemanticsHandler::TestBlockImpl> (block));
+      
+    s1::intermediate::SequencePtr newSeq (boost::make_shared<s1::intermediate::Sequence> ());
+    bool blocksInlined = s1::optimize::Inliner::InlineAllBlocks (newSeq, testBlockImpl->GetSequence());
+    TS_ASSERT_EQUALS (blocksInlined, true);
+    
+    TestSequenceVisitor testVisitor;
+    newSeq->Visit (testVisitor);
+    TS_ASSERT_EQUALS (testVisitor.entries.size(), 1);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].op, TestSequenceVisitor::opBranch);
+    
+    TS_ASSERT (testVisitor.entries[0].branchIfVisitor);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchIfVisitor->entries.size(), 1);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchIfVisitor->entries[0].op, TestSequenceVisitor::opBlock);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchIfVisitor->entries[0].blockVisitor->entries.size(), 1);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchIfVisitor->entries[0].blockVisitor->entries[0].op, TestSequenceVisitor::opConstUInt);
+    
+    TS_ASSERT (testVisitor.entries[0].branchElseVisitor);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchElseVisitor->entries.size(), 1);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchElseVisitor->entries[0].op, TestSequenceVisitor::opBlock);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchElseVisitor->entries[0].blockVisitor->entries.size(), 1);
+    TS_ASSERT_EQUALS (testVisitor.entries[0].branchElseVisitor->entries[0].blockVisitor->entries[0].op, TestSequenceVisitor::opConstUInt);
+  }
+  
 };
