1e9e9bae101973d34d74318507aec942100f7f55
Frank Richter <frank.richter@gmail.com>
2011-01-13 20:42:08 +0100

    Add section on optimizations

---
+++ thesis/gen_opt_ex.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+generate_common()
+{
+  sample=$1
+  opt=$2
+  s1cflags=$3
+  
+  S1C_LATEX=../../s1-build/apps/s1c/s1c_latex
+  $S1C_LATEX $3 -O0 s1source/opt_ex/${sample}.s1 > s1latex/opt_ex/${sample}_unopt.tex
+  $S1C_LATEX $3 -O0 $opt s1source/opt_ex/${sample}.s1 > s1latex/opt_ex/${sample}_opt.tex
+}
+
+generate()
+{
+  generate_common "$1" "$2" --nosplit
+}
+
+generate bi -Oinline-blocks
+generate cf -Oconstant-folding
+
+generate_common dce -Odce ""
