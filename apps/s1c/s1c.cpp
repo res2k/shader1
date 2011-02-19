271cd210bfbb029e1a7bba63d55202ba56103734
Frank Richter <frank.richter@gmail.com>
2011-02-19 17:09:47 +0100

    Change s1c to use 'Compiler' library

--- apps/s1c/s1c.cpp
+++ apps/s1c/s1c.cpp
@@ -3,22 +3,10 @@
 #include <fstream>
 #include <iostream>
 
-#include "base/UnicodeStream.h"
-#include "codegen/CgGenerator.h"
-#include "intermediate/Exception.h"
-#include "intermediate/IntermediateGeneratorSemanticsHandler.h"
-#include "intermediate/ProgramFunction.h" // @@@ Temp.
-#include "lexer/Lexer.h"
-#include "parser/Parser.h"
-#include "optimize/Optimizer.h"
-#include "splitter/Frequency.h"
-#include "splitter/ProgramSplitter.h"
+#include "compiler/Compiler.h"
+#include "compiler/Options.h"
+#include "compiler/Program.h"
 
-#include "ErrorHandler.h"
-#include "OptimizationFlags.h"
-
-#include <boost/make_shared.hpp>
-#include <boost/foreach.hpp>
 #include <string.h>
 
 using namespace s1;
@@ -32,10 +20,23 @@ int main (const int argc, const char* const argv[])
 {
   const char* inputFileName = 0;
   const char* entryName = "main";
+  const char* backendStr = "cg";
   bool doSplit = true;
   typedef boost::unordered_map<std::string, unsigned int> ParamMap;
   ParamMap paramFlags;
-  OptimizationFlags optFlags;
+  
+  Compiler compiler;
+  Compiler::OptionsPtr compilerOpts = compiler.CreateOptions();
+  enum
+  {
+    // Optimization level compiler picks by default
+    defaultOptimizationLevel = 1,
+    // Optimization level compiler picks if optimization is requested, but no particular options (-O)s
+    defaultEnableOptimizationLevel = 2
+  };
+  compilerOpts->SetOptimizationLevel (defaultOptimizationLevel);
+  
+  //OptimizationFlags optFlags;
   int argNum = 1;
   while (argNum < argc)
   {
@@ -70,11 +71,19 @@ int main (const int argc, const char* const argv[])
     }
     else if (strncmp (arg, "-O", 2) == 0)
     {
-      if (!optFlags.ParseFlag (arg+2))
-      {
-	PrintSyntax (argv[0]);
-	return 1;
-      }
+      const char* optStr = arg+2;
+      if (!*optStr)
+	compilerOpts->SetOptimizationLevel (defaultEnableOptimizationLevel);
+      else if ((*optStr >= '0') && (*optStr <= '9'))
+	compilerOpts->SetOptimizationLevel (*optStr - '0');
+      else
+	compilerOpts->SetOptimizationFlagFromStr (optStr);
+    }
+    else if (strcmp (arg, "--backend") == 0)
+    {
+      argNum++;
+      if (argNum < argc)
+	backendStr = argv[argNum];
     }
     else
     {
@@ -94,108 +103,52 @@ int main (const int argc, const char* const argv[])
     return 1;
   }
   
-  ErrorHandler errorHandler;
+  Compiler::BackendPtr compilerBackend;
+  if (strcasecmp (backendStr, "cg") == 0)
+    compilerBackend = compiler.CreateBackendCg();
+  else if (strcasecmp (backendStr, "latex") == 0)
+    compilerBackend = compiler.CreateBackendLatex();
+  else
+  {
+    std::cerr << "Invalid backend: " << backendStr << std::endl;
+  }
+  
   std::ifstream inputFile (inputFileName, std::ios_base::in | std::ios_base::binary);
-  UnicodeStream uniStream (inputFile, "utf-8");
-  // Skip BOM
-  if (*uniStream == 0xFEFF) ++uniStream;
-  
-  Lexer lexer (uniStream, errorHandler);
-  
-  intermediate::IntermediateGeneratorSemanticsHandler intermediateHandler;
-  intermediateHandler.SetEntryFunction (entryName);
-  Parser parser (lexer, intermediateHandler, errorHandler);
-  try
+  // Check for BOM
   {
-    parser.Parse ();
-  }
-  catch (intermediate::Exception& e)
-  {
-    errorHandler.IntermediateError (e.GetCode());
-  }
-  
-  optimize::Optimizer opt;
-  optFlags.ApplyFlags (opt);
-  
-  intermediate::ProgramPtr prog = intermediateHandler.GetProgram ();
-  // Determine output values
-  {
-    UnicodeString vertexOutput;
-    UnicodeString fragmentOutput;
-    
-    for (size_t i = 0; i < prog->GetNumFunctions(); i++)
+    unsigned char bomBuf[3];
+    inputFile.read ((char*)bomBuf, 3);
+    if (inputFile.eof() || (bomBuf[0] != 0xEF) || (bomBuf[1] != 0xBB) || (bomBuf[2] != 0xBF))
     {
-      intermediate::ProgramFunctionPtr func = prog->GetFunction (i);
-      if (!func->IsEntryFunction()) continue;
-      
-      const parser::SemanticsHandler::Scope::FunctionFormalParameters& funcParams = func->GetParams();
-      BOOST_FOREACH(const parser::SemanticsHandler::Scope::FunctionFormalParameter& param, funcParams)
-      {
-	if (!(param.dir & parser::SemanticsHandler::Scope::dirOut)) continue;
-	
-	// Look for float4 output
-	if (param.type->GetTypeClass() != parser::SemanticsHandler::Type::Vector) continue;
-	if (param.type->GetArrayVectorMatrixBaseType()->GetBaseType() != parser::SemanticsHandler::Float) continue;
-	if (param.type->GetVectorTypeComponents() != 4) continue;
-	
-	// Parameter qualifies
-	if (vertexOutput.isEmpty())
-	  vertexOutput = param.identifier;
-	else if (fragmentOutput.isEmpty())
-	  fragmentOutput = param.identifier;
-	else
-	{
-	  // Make 'proper' warning
-	  std::cerr << "Entry function has too many 'float4' outputs" << std::endl;
-	}
-      }
+      // Not a BOM, reset
+      inputFile.clear ();
+      inputFile.seekg (0);
     }
-    
-    if (vertexOutput.isEmpty() || fragmentOutput.isEmpty())
-    {
-      // Make 'proper' warning
-      std::cerr << "Entry function has not enough 'float4' outputs" << std::endl;
-    }
-    
-    prog->SetOutputParameter (vertexOutput, intermediate::Program::Position);
-    prog->SetOutputParameter (fragmentOutput, intermediate::Program::Color);
   }
-  prog = opt.ApplyOptimizations (prog);
+  Compiler::ProgramPtr compilerProg (compiler.CreateProgram (compilerOpts, inputFile));
+  compilerProg->SetEntryFunctionName (entryName);
   
+  for (ParamMap::const_iterator paramFlag = paramFlags.begin();
+	paramFlag != paramFlags.end();
+	++paramFlag)
+  {
+    compilerProg->SetInputParameterFrequencyFlags (paramFlag->first.c_str(), paramFlag->second);
+  }
+
   if (doSplit)
   {
-    splitter::ProgramSplitter splitter;
-
-    splitter.SetInputProgram (prog);
-    for (ParamMap::const_iterator paramFlag = paramFlags.begin();
-	  paramFlag != paramFlags.end();
-	  ++paramFlag)
-    {
-      splitter.SetInputFreqFlags (paramFlag->first.c_str(), paramFlag->second);
-    }
-    splitter.PerformSplit();
-
-    for (int f = 0; f < splitter::freqNum; f++)
-    {
-      prog = opt.ApplyOptimizations (splitter.GetOutputProgram (f));
-      codegen::CgGenerator codegen;
-      codegen::StringsArrayPtr progOutput (codegen.Generate (prog, f));
-      
-      for (size_t i = 0; i < progOutput->Size(); i++)
-      {
-	std::cout << progOutput->Get (i) << std::endl;
-      }
-    }
+    Compiler::Backend::ProgramPtr compiledVP (
+      compilerProg->GetCompiledProgram (compilerBackend, s1::Compiler::Backend::targetVP));
+    std::cout << compiledVP->GetProgramString () << std::endl;
+    Compiler::Backend::ProgramPtr compiledFP (
+      compilerProg->GetCompiledProgram (compilerBackend, s1::Compiler::Backend::targetFP));
+    std::cout << compiledFP->GetProgramString () << std::endl;
   }
   else
   {
-    codegen::CgGenerator codegen;
-    codegen::StringsArrayPtr progOutput (codegen.Generate (prog, -1));
-    
-    for (size_t i = 0; i < progOutput->Size(); i++)
-    {
-      std::cout << progOutput->Get (i) << std::endl;
-    }
+    Compiler::Backend::ProgramPtr compiledProgram (
+      compilerProg->GetCompiledProgram (compilerBackend, s1::Compiler::Backend::targetUnsplit));
+    std::cout << compiledProgram->GetProgramString () << std::endl;
   }
   
   return 0;
