9789db159fc5c36fadc4bdf8206054f0ee04889e
Frank Richter <frank.richter@gmail.com>
2011-02-21 22:58:59 +0100

    Switch from s1c_latex to s1c --backend latex

--- thesis/s1src2latex.sh
+++ thesis/s1src2latex.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for f in s1source/*.s1; do
   echo $f: 
-  ../../s1-build/apps/s1c/s1c_latex --nosplit -O0 $f > s1latex/`basename $f .s1`.tex
+  ../../s1-build/apps/s1c/s1c --backend latex --nosplit -O0 $f > s1latex/`basename $f .s1`.tex
 done
