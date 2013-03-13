920a96ddceb07eb2958038243681a7780651394f
Frank Richter <frank.richter@gmail.com>
2013-03-14 00:37:16 +0100

    Autogenerate public API Object header

---
+++ build/generate_preprocessor_h.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+collect_included()
+{
+  header_file="$1"
+  shift
+  gcc -x c-header -E "$header_file" "$@" \
+    | grep -v "$header_file" \
+    | grep "^#" \
+    | tail -n +2 \
+    | sed -e 's/.*"\(.*\)".*/\1/' \
+    | tac \
+    | awk ' !x[$0]++' \
+    | tac
+}
+
+for f in $(collect_included "$@"); do
+  cat $f | grep -v "^#.*include"
+done
