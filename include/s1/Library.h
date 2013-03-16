f3f6746604051c0ae31dd01f2e991519ae4b805c
Frank Richter <frank.richter@gmail.com>
2013-03-16 18:28:35 +0100

    Add 'Library' type object

---
+++ include/s1/Library.h
@@ -0,0 +1,51 @@
+/**\file
+ * Main library entry object
+ */
+#ifndef __S1_LIBRARY_H__
+#define __S1_LIBRARY_H__
+
+#include "s1/Error.h"
+#include "s1/Object.h"
+#include "s1/Ptr.h"
+
+#define S1TYPE_INFO_s1_Library   (s1_Library, S1TYPE_INFO_s1_Object)
+S1TYPE_DECLARE(s1_Library);
+
+/**
+ * Create a library object. A library object generally serves as a factory
+ * for all available object types.
+ */
+S1_API s1_ErrorCode s1_create_library (s1_Library** out);
+
+#if defined(__cplusplus)
+namespace s1
+{
+  namespace cxxapi
+  {
+    class Library : public s1_Library,
+                    public Object
+    {
+    public:
+      typedef Ptr<Library> Pointer;
+
+      static ErrorCode Create (Pointer& lib) throw()
+      {
+        s1_Library* p (0);
+        ErrorCode err = s1_create_library (&p);
+        if (S1_SUCCESSFUL(err))
+        {
+          lib.take (static_cast<Library*> (p));
+        }
+        return err;
+      }
+    };
+  } // namespace cxxapi
+
+#if !defined(S1_BUILD)
+  typedef cxxapi::Library Library;
+#endif // !defined(S1_BUILD)
+} // namespace s1
+
+#endif // defined(__cplusplus)
+
+#endif // __S1_LIBRARY_H__
---
+++ lib/base/Library.cpp
@@ -0,0 +1,28 @@
+#include "s1/Library.h"
+
+#include "base/Object.h"
+
+#include <new>
+
+namespace s1
+{
+  class Library : public s1_Library,
+                  public Object
+  {
+  public:
+    Library() {}
+    
+    // TODO: Store stuff like memory allocator, global options etc... here
+  }; 
+}
+
+s1_ErrorCode s1_create_library (s1_Library** out)
+{
+  if (!out) return S1_E_INVALID_ARG_N(0);
+  
+  s1::Library* new_lib (new (std::nothrow) s1::Library);
+  if (!new_lib) return S1_E_OUT_OF_MEMORY;
+  new_lib->AddRef();
+  *out = new_lib;
+  return S1_SUCCESS;
+}
