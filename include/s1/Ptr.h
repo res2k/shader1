089d35767b741b72f3d20ed2f93dae35e4e74a6d
Frank Richter <frank.richter@gmail.com>
2013-04-14 14:50:07 +0200

    Add separate smart pointer class for C API pointers

--- include/s1/Ptr.h
+++ include/s1/Ptr.h
@@ -9,6 +9,80 @@
 #ifdef __cplusplus
 namespace s1
 {
+  /// Smart pointer for Shader1 C public API objects.
+  template<typename T>
+  class CPtr
+  {
+  private:
+    T* obj;
+  public:
+    struct TakeReference {};
+    
+    CPtr () : obj (0) {}
+    CPtr (T* p) : obj (p)
+    {
+      s1_add_ref (S1TYPE_CAST (p, s1_Object));
+    }
+    CPtr (T* p, TakeReference) : obj (p) { }
+    CPtr (const CPtr& other) : obj (0) { reset (other.obj); }
+    ~CPtr()
+    {
+      if (obj) s1_release (S1TYPE_CAST (obj, s1_Object));
+    }
+    
+    void reset (T* p = 0)
+    {
+      if (p) s1_add_ref (S1TYPE_CAST (p, s1_Object));
+      if (obj) s1_release (S1TYPE_CAST (obj, s1_Object));
+      obj = p;
+    }
+    /**
+     * Assume ownership of a pointer.
+     * Replaces the currently stored pointer. Properly releases reference of the
+     * old pointer, but does not add a reference to the new pointer!
+     */
+    void take (T* p)
+    {
+      if (obj) s1_release (S1TYPE_CAST (obj, s1_Object));
+      obj = p;
+    }
+    T* get() const
+    {
+      return obj;
+    }
+    operator T* () const
+    {
+      return obj;
+    }
+    /**
+     * Hand over ownership of a pointer.
+     * Returns the stored pointer and clears it internally.
+     * Does not release a reference to the pointer!
+     */
+    T* detach()
+    {
+      T* p (obj);
+      obj = 0;
+      return p;
+    }
+    
+    CPtr& operator= (const CPtr& other)
+    {
+      reset (other.obj);
+      return *this;
+    }
+
+    T& operator* () const
+    {
+      return *obj;
+    }
+    T* operator-> () const
+    {
+      return obj;
+    }
+  };
+
+  /// Smart pointer for Shader1 C++ public API objects.
   template<typename T>
   class Ptr
   {
@@ -23,7 +97,14 @@ namespace s1
       s1_add_ref (p->Cpointer());
     }
     Ptr (T* p, TakeReference) : obj (p) { }
+    Ptr (typename T::CType* p) : obj (T::FromC (p))
+    {
+      s1_add_ref (p);
+    }
+    Ptr (typename T::CType* p, TakeReference) : obj (T::FromC (p)) { }
     Ptr (const Ptr& other) : obj (0) { reset (other.obj); }
+    template<typename T2>
+    Ptr (const CPtr<T2>& other) : obj (0) { reset (T::FromC (other)); }
     ~Ptr()
     {
       if (obj) s1_release (obj->Cpointer());
@@ -34,6 +115,12 @@ namespace s1
       if (p) s1_add_ref (p->Cpointer());
       if (obj) s1_release (obj->Cpointer());
       obj = p;
+    }
+    void reset (typename T::CType* p)
+    {
+      if (p) s1_add_ref (p);
+      if (obj) s1_release (obj->Cpointer());
+      obj = T::FromC (p);
     }
     /**
      * Assume ownership of a pointer.
@@ -69,6 +156,22 @@ namespace s1
     {
       reset (other.obj);
       return *this;
+    }
+    template<typename T2>
+    Ptr& operator= (const CPtr<T2>& other)
+    {
+      reset (T::FromC (other));
+      return *this;
+    }
+    Ptr& operator= (typename T::CType* other)
+    {
+      reset (other);
+      return *this;
+    }
+
+    operator CPtr<typename T::CType> () const
+    {
+      return CPtr<typename T::CType> (obj->Cpointer());
     }
 
     T& operator* () const
