e790dd92cb500eb4c2418f1b54c7a5beb4fd13dd
Frank Richter <frank.richter@gmail.com>
2013-05-03 22:11:57 +0200

    Add convenience methods for the often-used 'set error code, return value' pattern

--- include/internal/base/LibraryObject.h
+++ include/internal/base/LibraryObject.h
@@ -16,6 +16,43 @@ namespace s1
     LibraryObject (Library* lib) : lib (lib) {}
     
     Library* GetLibrary() { return lib.get(); }
+    
+    /**
+     * Helper method to to set a library error code and return an appropriate
+     * \c s1_bool result.
+     */
+    inline s1_bool ReturnErrorCode (s1_ErrorCode code)
+    {
+      GetLibrary()->SetLastError (code);
+      return S1_SUCCESSFUL(code);
+    }
+    /**
+     * Helper method to to set the library error code to #S1_SUCCESS
+     * and return the appropriate \c s1_bool result.
+     */
+    inline s1_bool ReturnSuccess ()
+    {
+      return ReturnErrorCode (S1_SUCCESS);
+    }
+
+    /**
+     * Helper method to to set a library error code and return an arbitrary result.
+     */
+    template<typename T>
+    inline const T& ReturnErrorCode (s1_ErrorCode code, const T& result)
+    {
+      GetLibrary()->SetLastError (code);
+      return result;
+    }
+    /**
+     * Helper method to to set the library error code to #S1_SUCCESS
+     * and return an arbitrary result.
+     */
+    template<typename T>
+    inline const T& ReturnSuccess (const T& result)
+    {
+      return ReturnErrorCode (S1_SUCCESS, result);
+    }
   }; 
 } // namespace s1
 
