1e729d1da9361a7399d7988f534d39a2e178f63b
Frank Richter <frank.richter@gmail.com>
2013-07-22 22:20:41 +0200

    internal/intermediate: Generate proper InOut parameters for global vars, no multiple vars with the same identifier

--- lib/intermediate/FunctionImpl.cpp
+++ lib/intermediate/FunctionImpl.cpp
@@ -15,6 +15,9 @@ namespace s1
       boost::shared_ptr<BlockImpl> funcBlockImpl (boost::static_pointer_cast<BlockImpl> (block));
       funcBlockImpl->FinishBlock ();
 
+      typedef boost::unordered_map<NameImplPtr, Scope::FunctionFormalParameter> AutoGlobalParamsMap;
+      AutoGlobalParamsMap autoGlobalParams;
+
       // Add artificial arguments for global variables
       Sequence::RegisterImpMappings imports (funcBlockImpl->GetSequence()->GetImports());
       for (Sequence::RegisterImpMappings::const_iterator imported (imports.begin());
@@ -33,7 +36,7 @@ namespace s1
           newParam.type = global->valueType;
           newParam.identifier = imported->first;
           newParam.defaultValue = nameExpr;
-          funcInfo->params.push_back (newParam);
+          autoGlobalParams[global] = newParam;
         }
       }
 
@@ -45,17 +48,31 @@ namespace s1
         NameImplPtr global (handler->globalScope->ResolveIdentifierInternal (exported->first));
         if (global)
         {
-          // Synthesize an expression to assign the global with the default value
-          ExpressionPtr nameExpr (handler->CreateVariableExpression (global));
-          // Synthesize function parameter
-          Scope::FunctionFormalParameter newParam;
-          newParam.paramType = Scope::ptAutoGlobal;
-          newParam.dir = Scope::dirOut;
-          newParam.type = global->valueType;
-          newParam.identifier = exported->first;
-          newParam.defaultValue = nameExpr;
-          funcInfo->params.push_back (newParam);
+          AutoGlobalParamsMap::iterator map_it (autoGlobalParams.find (global));
+          if (map_it != autoGlobalParams.end())
+          {
+            // The param is both an input and output param, augment the existing param
+            map_it->second.dir = Scope::dirInOut;
+          }
+          else
+          {
+            // Synthesize an expression to assign the global with the default value
+            ExpressionPtr nameExpr (handler->CreateVariableExpression (global));
+            // Synthesize function parameter
+            Scope::FunctionFormalParameter newParam;
+            newParam.paramType = Scope::ptAutoGlobal;
+            newParam.dir = Scope::dirOut;
+            newParam.type = global->valueType;
+            newParam.identifier = exported->first;
+            newParam.defaultValue = nameExpr;
+            autoGlobalParams[global] = newParam;
+          }
         }
+      }
+
+      BOOST_FOREACH(const AutoGlobalParamsMap::value_type& newParam, autoGlobalParams)
+      {
+        funcInfo->params.push_back (newParam.second);
       }
     }
   } // namespace intermediate
