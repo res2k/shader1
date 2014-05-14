ffcbfed9833300ce14d9668de130d201be3ec391
Frank Richter <frank.richter@gmail.com>
2014-05-14 09:15:21 +0200

    Changed licence to wxWidgets Library License.

--- include/internal/intermediate/SequenceOp/SequenceOp.h
+++ include/internal/intermediate/SequenceOp/SequenceOp.h
@@ -1,3 +1,20 @@
+/*
+    Shader1
+    Copyright (c) 2010-2014 Frank Richter
+
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the wxWindows Library Licence.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    Please see the wxWindows Library Licence for more details.
+    The licence texts can be found in the source distribution files
+    LICENCE-wxWindows.txt and LICENCE-LGPL.txt.
+*/
+
 #ifndef __INTERMEDIATE_SEQUENCEOP_H__
 #define __INTERMEDIATE_SEQUENCEOP_H__
 
