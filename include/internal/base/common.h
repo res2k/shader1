665d03b0a18ab0ab6ace90628716964658f5703f
Frank Richter <frank.richter@gmail.com>
2014-05-21 13:55:08 +0200

    internal/base: Macro for digit approximation

--- include/internal/base/common.h
+++ include/internal/base/common.h
@@ -25,13 +25,18 @@
 #include "base/config.h"
 #include <boost/cstdint.hpp> // must be include before any unicode/ headers to avoid macro conflict
 
+/**\def S1_APPROX_DIGITS(Type)
+ * Approximate number of characters needed to display a value of type
+ * \a Type in decimal system.
+ */
 /* (sizeof(x)*25)/10+1 is an approximation of the number of characters
   * needed to display x in decimal system. (x can be at most 256^sizeof(x).
   * You need log10(256^sizeof(x)) characters, becoming
   * sizeof(x)*log10(256). 25/10 is an (over-)approximation of log10(256).) */
-static const size_t charsToFormatUint = (sizeof(unsigned int) * 25) / 10;
+#define S1_APPROX_DIGITS(Type)          ((sizeof(Type) * 25) / 10)
+static const size_t charsToFormatUint = S1_APPROX_DIGITS(unsigned int);
 // Add 1 for '-'
-static const size_t charsToFormatInt = (sizeof(unsigned int) * 25) / 10 + 1;
+static const size_t charsToFormatInt = S1_APPROX_DIGITS(unsigned int) + 1;
 
 // Used all over the place
 #include "uc/String.h"
