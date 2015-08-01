02ad4d2678a650ff32cc683f29891d84aefb4135
Frank Richter <frank.richter@gmail.com>
2015-08-01 13:03:12 +0200

    Build libraries using OBJECT_LIBRARY instead of merging static libraries

---
+++ cmake/s1_add_cxxtest.cmake
@@ -0,0 +1,20 @@
+FUNCTION(s1_add_cxxtest TARGET)
+  CMAKE_PARSE_ARGUMENTS(S1_ADD_CXXTEST
+    ""
+    ""
+    "SOURCES;DEPENDS"
+    ${ARGN})
+  S1_GET_DEPENDENCIES(add_sources add_libs ${S1_ADD_CXXTEST_DEPENDS})
+  CXXTEST_ADD_TEST(${TARGET} ${TARGET}.cpp
+                   ${S1_ADD_CXXTEST_SOURCES})
+  IF(add_sources)
+    ADD_LIBRARY(${TARGET}_dep STATIC ${add_sources})
+    set_property(TARGET ${TARGET}_dep PROPERTY EXCLUDE_FROM_ALL YES)
+    target_link_libraries(${TARGET} ${TARGET}_dep)
+  ENDIF()
+  IF(add_libs)
+    target_link_libraries(${TARGET} ${add_libs})
+  ENDIF()
+  set_property(TARGET ${TARGET} PROPERTY EXCLUDE_FROM_ALL YES)
+  AutoBuildTest(${TARGET})
+ENDFUNCTION()
