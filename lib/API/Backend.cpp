06efe350f58f63bb2ff053ddee780c9e166d6832
Frank Richter <frank.richter@gmail.com>
2015-11-17 18:20:28 +0100

    API: Use Try() in s1_backend_generate_program()

--- lib/API/Backend.cpp
+++ lib/API/Backend.cpp
@@ -31,32 +31,36 @@ s1_CompiledProgram* s1_backend_generate_program (s1_Backend* backend,
   S1_ASSERT_MSG(backend, "NULL Backend", nullptr);
   s1::Compiler::Backend* backend_impl (s1::EvilUpcast<s1::Compiler::Backend> (backend));
 
-  if (!program)
-  {
-    return backend_impl->ReturnErrorCode (S1_E_INVALID_ARG_N (0), nullptr);
-  }
-  s1::api_impl::Program* program_impl (s1::EvilUpcast<s1::api_impl::Program> (program));
-  
-  s1::Compiler::Backend::CompileTarget impl_target;
-  switch (target)
-  {
-  case S1_TARGET_VP:        impl_target = s1::Compiler::Backend::targetVP; break;
-  case S1_TARGET_FP:        impl_target = s1::Compiler::Backend::targetFP; break;
-  case S1_TARGET_UNSPLIT:   impl_target = s1::Compiler::Backend::targetUnsplit; break;
-  default:
-    return backend_impl->ReturnErrorCode (S1_E_INVALID_ARG_N (1), nullptr);
-  }
+  return backend_impl->Return (backend_impl->Try (
+    [=]() -> s1::Result<s1_CompiledProgram*>
+    {
+      if (!program)
+      {
+        return S1_E_INVALID_ARG_N (0);
+      }
+      s1::api_impl::Program* program_impl (s1::EvilUpcast<s1::api_impl::Program> (program));
 
-  auto options_impl (s1::EvilUpcast<s1::Compiler::Backend::Options> (options));
-  s1::Compiler::Backend::ProgramPtr compiled_program (
-    program_impl->GetCompiledProgram (backend_impl, impl_target, options_impl));
-  if (!compiled_program)
-  {
-    return backend_impl->ReturnErrorCode (S1_E_GENERATE_FAILED, nullptr);
-  }
-  
-  compiled_program->AddRef();
-  return backend_impl->ReturnSuccess (compiled_program->DowncastEvil<s1_CompiledProgram> ());
+      s1::Compiler::Backend::CompileTarget impl_target;
+      switch (target)
+      {
+      case S1_TARGET_VP:        impl_target = s1::Compiler::Backend::targetVP; break;
+      case S1_TARGET_FP:        impl_target = s1::Compiler::Backend::targetFP; break;
+      case S1_TARGET_UNSPLIT:   impl_target = s1::Compiler::Backend::targetUnsplit; break;
+      default:
+        return S1_E_INVALID_ARG_N (1);
+      }
+
+      auto options_impl (s1::EvilUpcast<s1::Compiler::Backend::Options> (options));
+      s1::Compiler::Backend::ProgramPtr compiled_program (
+        program_impl->GetCompiledProgram (backend_impl, impl_target, options_impl));
+      if (!compiled_program)
+      {
+        return S1_E_GENERATE_FAILED;
+      }
+
+      compiled_program->AddRef ();
+      return compiled_program->DowncastEvil<s1_CompiledProgram> ();
+    }), nullptr);
 }
 
 s1_BackendOptions* s1_backendoptions_create (s1_Backend* backend)
