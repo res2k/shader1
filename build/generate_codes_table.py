dea65de5295baefe2de70011cc91915585fa89f0
Frank Richter <frank.richter@gmail.com>
2017-12-01 00:19:59 +0100

    build: Add ability to pass arbitrary values to template to generate_codes_table.py

--- build/generate_codes_table.py
+++ build/generate_codes_table.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(description='Generate some file from a error/re
 parser.add_argument('input_file', metavar='XML-FILE', nargs='+', help='input XML file')
 parser.add_argument('--template', '-t', dest='template_file', metavar='TEMPLATE-FILE', help='results template file', required=True)
 parser.add_argument('--root', '-r', metavar='ROOT-ID', help='identifier of root data container used in template', required=True)
+parser.add_argument('-D', dest='defines', metavar='KEY=VALUE', nargs='*', help='additional values passed to template')
 
 root_children = []
 args = parser.parse_args()
@@ -23,6 +24,10 @@ template = tempita.Template.from_filename(args.template_file,
 
 last_empty = False
 subst_args = { args.root: root_children }
+if args.defines:
+    for d in args.defines:
+        key, val = d.split('=', 1)
+        subst_args[key] = val
 for line in template.substitute(subst_args).splitlines():
     if not line.strip():
         if not last_empty:
