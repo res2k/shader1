fb9e74403561c4c9bb919a617a982d92c6c35825
Frank Richter <frank.richter@gmail.com>
2017-12-01 13:37:50 +0100

    diagnostics: Add very basic 'diagnostics handler' interface

---
+++ include/internal/diagnostics/Handler.h
@@ -0,0 +1,46 @@
+/*
+    Shader1
+    Copyright (c) 2017 Frank Richter
+
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the wxWindows Library Licence.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    Please see the wxWindows Library Licence for more details.
+    The licence texts can be found in the source distribution files
+    LICENCE-wxWindows.txt and LICENCE-LGPL.txt.
+*/
+
+/**\file
+ * Diagnostics handler interface
+ */
+#ifndef DIAGNOSTICS_HANDLER_H__
+#define DIAGNOSTICS_HANDLER_H__
+
+#include <type_traits>
+
+namespace s1
+{
+  namespace diagnostics
+  {
+    struct Handler
+    {
+      virtual ~Handler() {}
+
+      /// Semantic errors: Usually detected during intermediate generation
+      template<typename T>
+      typename std::enable_if<std::is_enum<T>::value>::type SemanticError (T code)
+      {
+        SemanticErrorImpl (static_cast<unsigned int> (code));
+      }
+    protected:
+      virtual void SemanticErrorImpl (unsigned int code) = 0;
+    };
+  } // namespace diagnostics
+} // namespace s1
+
+#endif // DIAGNOSTICS_HANDLER_H__
---
+++ include/internal/diagnostics/forwarddecl.h
@@ -0,0 +1,32 @@
+/*
+    Shader1
+    Copyright (c) 2017 Frank Richter
+
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the wxWindows Library Licence.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    Please see the wxWindows Library Licence for more details.
+    The licence texts can be found in the source distribution files
+    LICENCE-wxWindows.txt and LICENCE-LGPL.txt.
+*/
+
+/**\file
+ * 'diagnostics' forward declarations
+ */
+#ifndef DIAGNOSTICS_FORWARDDECL_H__
+#define DIAGNOSTICS_FORWARDDECL_H__
+
+namespace s1
+{
+  namespace diagnostics
+  {
+    struct Handler;
+  } // namespace diagnostics
+} // namespace s1
+
+#endif // DIAGNOSTICS_FORWARDDECL_H__
