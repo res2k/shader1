611438dff6814bdb020e54cecdfe8c94b21c86ed
Frank Richter <frank.richter@gmail.com>
2017-12-07 14:26:27 +0100

    parser: Convert tabs to spaces

--- include/internal/parser/CommonSemanticsHandler.h
+++ include/internal/parser/CommonSemanticsHandler.h
@@ -36,46 +36,46 @@ namespace s1
     protected:
       struct CommonType : public Type
       {
-	Class typeClass;
-	BaseType base;
-	SamplerType sampler;
-	TypePtr avmBase;
-	unsigned int vectorDim;
-	unsigned int matrixCols;
-	unsigned int matrixRows;
-	
-	CommonType (BaseType base) : typeClass (Base), base (base) {}
-	CommonType (SamplerType sampler) : typeClass (Sampler), sampler (sampler) {}
-	CommonType (TypePtr aBase) : typeClass (Array), avmBase (aBase) {}
-	CommonType (TypePtr vBase, unsigned int d)
-	 : typeClass (Vector), avmBase (vBase), vectorDim (d) {}
-	CommonType (TypePtr mBase, unsigned int c, unsigned int r)
-	 : typeClass (Matrix), avmBase (mBase), matrixCols (c), matrixRows (r) {}
-	
-	Class GetTypeClass() const { return typeClass; }
-	BaseType GetBaseType() const { return base; }
-	SamplerType GetSamplerType() const { return sampler; }
-	
-	TypePtr GetArrayVectorMatrixBaseType() const { return avmBase; }
-	
-	unsigned int GetVectorTypeComponents() const { return vectorDim; }
-	
-	unsigned int GetMatrixTypeCols() const { return matrixCols; }
-	unsigned int GetMatrixTypeRows() const { return matrixRows; }
-	
-	/// Returns whether this type is losslessly assignable to \a to.
-	bool CompatibleLossless (const CommonType& to) const;
-	bool CompatibleLossy (const s1::parser::CommonSemanticsHandler::CommonType& to) const;
-	bool IsEqual (const CommonType& other) const;
-	bool IsPrecisionHigherEqual (const CommonType& other) const;
-	
-	uc::String ToString() const;
+        Class typeClass;
+        BaseType base;
+        SamplerType sampler;
+        TypePtr avmBase;
+        unsigned int vectorDim;
+        unsigned int matrixCols;
+        unsigned int matrixRows;
+        
+        CommonType (BaseType base) : typeClass (Base), base (base) {}
+        CommonType (SamplerType sampler) : typeClass (Sampler), sampler (sampler) {}
+        CommonType (TypePtr aBase) : typeClass (Array), avmBase (aBase) {}
+        CommonType (TypePtr vBase, unsigned int d)
+         : typeClass (Vector), avmBase (vBase), vectorDim (d) {}
+        CommonType (TypePtr mBase, unsigned int c, unsigned int r)
+         : typeClass (Matrix), avmBase (mBase), matrixCols (c), matrixRows (r) {}
+        
+        Class GetTypeClass() const { return typeClass; }
+        BaseType GetBaseType() const { return base; }
+        SamplerType GetSamplerType() const { return sampler; }
+        
+        TypePtr GetArrayVectorMatrixBaseType() const { return avmBase; }
+        
+        unsigned int GetVectorTypeComponents() const { return vectorDim; }
+        
+        unsigned int GetMatrixTypeCols() const { return matrixCols; }
+        unsigned int GetMatrixTypeRows() const { return matrixRows; }
+        
+        /// Returns whether this type is losslessly assignable to \a to.
+        bool CompatibleLossless (const CommonType& to) const;
+        bool CompatibleLossy (const s1::parser::CommonSemanticsHandler::CommonType& to) const;
+        bool IsEqual (const CommonType& other) const;
+        bool IsPrecisionHigherEqual (const CommonType& other) const;
+        
+        uc::String ToString() const;
       };
       
       /**\name Type utilities
        * @{ */
       static boost::shared_ptr<CommonType> GetHigherPrecisionType (
-	const boost::shared_ptr<CommonType>& t1, const boost::shared_ptr<CommonType>& t2);
+        const boost::shared_ptr<CommonType>& t1, const boost::shared_ptr<CommonType>& t2);
       static BaseType DetectNumericType (const uc::String& numericStr);
       /**@}*/
       
@@ -83,78 +83,78 @@ namespace s1
        * @{ */
       struct Attribute
       {
-	enum AttrClass
-	{
-	  Unknown,
-	  
-	  arrayLength,
-	  
-	  matrixRow,
-	  matrixCol,
-	  matrixTranspose,
-	  matrixInvert,
-	  
-	  vectorSwizzle
-	};
-	AttrClass attrClass;
-	unsigned char swizzleCompNum;
-	unsigned char swizzleComps;
-	
-	Attribute (AttrClass ac) : attrClass (ac), swizzleCompNum (0), swizzleComps (0) {}
-	Attribute (unsigned char swizNum, unsigned char comp1 = 0, unsigned char comp2 = 0,
-		   unsigned char comp3 = 0, unsigned char comp4 = 0)
-	 : attrClass (vectorSwizzle), swizzleCompNum (swizNum),
-	   swizzleComps ((comp1 & 3) | ((comp2 & 3) << 2) | ((comp3 & 3) << 4) | ((comp4 & 3) << 6))
-	{}
+        enum AttrClass
+        {
+          Unknown,
+          
+          arrayLength,
+          
+          matrixRow,
+          matrixCol,
+          matrixTranspose,
+          matrixInvert,
+          
+          vectorSwizzle
+        };
+        AttrClass attrClass;
+        unsigned char swizzleCompNum;
+        unsigned char swizzleComps;
+        
+        Attribute (AttrClass ac) : attrClass (ac), swizzleCompNum (0), swizzleComps (0) {}
+        Attribute (unsigned char swizNum, unsigned char comp1 = 0, unsigned char comp2 = 0,
+                   unsigned char comp3 = 0, unsigned char comp4 = 0)
+         : attrClass (vectorSwizzle), swizzleCompNum (swizNum),
+           swizzleComps ((comp1 & 3) | ((comp2 & 3) << 2) | ((comp3 & 3) << 4) | ((comp4 & 3) << 6))
+        {}
       };
       static Attribute IdentifyAttribute (const uc::String& attributeStr);
       TypePtr GetAttributeType (const boost::shared_ptr<CommonType>& expressionType,
-				const Attribute& attr);
+                                const Attribute& attr);
       /** @} */
       
       struct CommonName : public Name
       {
-	uc::String identifier;
-	NameType type;
-	
-	/* Variables/Constants: type of variable/constant
-	 * Functions: type of return value
-	 * Type aliases: aliased type
-	 */
-	TypePtr valueType;
-	// Variables/Constants: value
-	ExpressionPtr varValue;
-	// Distinguish between variable/constant
-	bool varConstant;
-	
-	CommonName (const uc::String& identifier, NameType type, TypePtr typeOfName)
-	 : identifier (identifier), type (type), valueType (typeOfName) {}
-	CommonName (const uc::String& identifier, TypePtr typeOfName,
-		    ExpressionPtr value, bool constant)
-	 : identifier (identifier), type (Variable), valueType (typeOfName),
-	   varValue (value), varConstant (constant) {}
-	
-	NameType GetType() { return type; }
-	TypePtr GetAliasedType()
-	{ return type == TypeAlias ? valueType : TypePtr (); }
+        uc::String identifier;
+        NameType type;
+        
+        /* Variables/Constants: type of variable/constant
+         * Functions: type of return value
+         * Type aliases: aliased type
+         */
+        TypePtr valueType;
+        // Variables/Constants: value
+        ExpressionPtr varValue;
+        // Distinguish between variable/constant
+        bool varConstant;
+        
+        CommonName (const uc::String& identifier, NameType type, TypePtr typeOfName)
+         : identifier (identifier), type (type), valueType (typeOfName) {}
+        CommonName (const uc::String& identifier, TypePtr typeOfName,
+                    ExpressionPtr value, bool constant)
+         : identifier (identifier), type (Variable), valueType (typeOfName),
+           varValue (value), varConstant (constant) {}
+        
+        NameType GetType() { return type; }
+        TypePtr GetAliasedType()
+        { return type == TypeAlias ? valueType : TypePtr (); }
         const uc::String& GetIdentifier () { return identifier; }
         bool IsConstantVariable () { return (type == Variable) && varConstant; }
         TypePtr GetValueType () { return valueType; }
       };
 
       class CommonScope : public Scope,
-			  public boost::enable_shared_from_this<CommonScope>
+                          public boost::enable_shared_from_this<CommonScope>
       {
-	friend class CommonSemanticsHandler;
-	
-	typedef boost::unordered_map<uc::String, NamePtr> IdentifierMap;
-	IdentifierMap identifiers;
-	
-	void CheckIdentifierUnique (const uc::String& identifier);
-	
-	CommonSemanticsHandler* handler;
-	boost::shared_ptr<CommonScope> parent;
-	ScopeLevel level;
+        friend class CommonSemanticsHandler;
+        
+        typedef boost::unordered_map<uc::String, NamePtr> IdentifierMap;
+        IdentifierMap identifiers;
+        
+        void CheckIdentifierUnique (const uc::String& identifier);
+        
+        CommonSemanticsHandler* handler;
+        boost::shared_ptr<CommonScope> parent;
+        ScopeLevel level;
 
         class CommonFunction : public Function
         {
@@ -165,23 +165,23 @@ namespace s1
           void Finish() {}
         };
       public:
-	CommonScope (CommonSemanticsHandler* handler,
-		     const boost::shared_ptr<CommonScope>& parent, ScopeLevel level);
-	ScopeLevel GetLevel() const { return level; }
-	
-	NamePtr AddVariable (TypePtr type,
-	  const uc::String& identifier,
-	  ExpressionPtr initialValue,
-	  bool constant);
-	  
-	NamePtr AddTypeAlias (TypePtr aliasedType,
-	  const uc::String& identifier);
-	  
-	FunctionPtr AddFunction (TypePtr returnType,
-	  const uc::String& identifier,
-	  const FunctionFormalParameters& params);
+        CommonScope (CommonSemanticsHandler* handler,
+                     const boost::shared_ptr<CommonScope>& parent, ScopeLevel level);
+        ScopeLevel GetLevel() const { return level; }
+        
+        NamePtr AddVariable (TypePtr type,
+          const uc::String& identifier,
+          ExpressionPtr initialValue,
+          bool constant);
+          
+        NamePtr AddTypeAlias (TypePtr aliasedType,
+          const uc::String& identifier);
+          
+        FunctionPtr AddFunction (TypePtr returnType,
+          const uc::String& identifier,
+          const FunctionFormalParameters& params);
       
-	NamePtr ResolveIdentifier (const uc::String& identifier);
+        NamePtr ResolveIdentifier (const uc::String& identifier);
       };
       
     public:  
@@ -189,10 +189,10 @@ namespace s1
       TypePtr CreateSamplerType (SamplerType dim);
       TypePtr CreateArrayType (TypePtr baseType);
       TypePtr CreateVectorType (TypePtr baseType,
-				unsigned int components);
+                                unsigned int components);
       TypePtr CreateMatrixType (TypePtr baseType,
-				unsigned int columns,
-				unsigned int rows);
+                                unsigned int columns,
+                                unsigned int rows);
       
       ScopePtr CreateScope (ScopePtr parentScope, ScopeLevel scopeLevel);
     };
