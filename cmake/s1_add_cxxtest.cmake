d6a07b78bd34bc6ae23d95178fc85abee61aed6d
Frank Richter <frank.richter@gmail.com>
2018-03-07 19:35:08 +0100

    Add variant of CXXTEST_ADD_TEST supporting generator expressions

--- cmake/s1_add_cxxtest.cmake
+++ cmake/s1_add_cxxtest.cmake
@@ -1,3 +1,20 @@
+# This is CXXTEST_ADD_TEST, modified to support generator expressions
+macro(S1_CXXTEST_ADD_TEST _cxxtest_testname _cxxtest_outfname)
+    set(_cxxtest_real_outfname ${CMAKE_CURRENT_BINARY_DIR}/${_cxxtest_outfname})
+
+    add_custom_command(
+        OUTPUT  ${_cxxtest_real_outfname}
+        DEPENDS ${ARGN}
+        COMMAND ${CXXTEST_TESTGEN_INTERPRETER}
+        ${CXXTEST_TESTGEN_EXECUTABLE} ${CXXTEST_TESTGEN_ARGS} -o ${_cxxtest_real_outfname} ${ARGN}
+    )
+
+    set_source_files_properties(${_cxxtest_real_outfname} PROPERTIES GENERATED true)
+    add_executable(${_cxxtest_testname} ${_cxxtest_real_outfname} ${ARGN})
+
+    add_test(NAME ${_cxxtest_testname} COMMAND ${_cxxtest_testname})
+endmacro()
+
 FUNCTION(s1_add_cxxtest TARGET)
   CMAKE_PARSE_ARGUMENTS(S1_ADD_CXXTEST
     ""
@@ -5,8 +22,8 @@ FUNCTION(s1_add_cxxtest TARGET)
     "SOURCES;DEPENDS"
     ${ARGN})
   S1_GET_DEPENDENCIES(add_sources add_libs add_targets ${S1_ADD_CXXTEST_DEPENDS})
-  CXXTEST_ADD_TEST(${TARGET} ${TARGET}.cpp
-                   ${S1_ADD_CXXTEST_SOURCES})
+  S1_CXXTEST_ADD_TEST(${TARGET} ${TARGET}.cpp
+                      ${S1_ADD_CXXTEST_SOURCES})
   IF(add_sources)
     ADD_LIBRARY(${TARGET}_dep STATIC ${add_sources})
     set_property(TARGET ${TARGET}_dep PROPERTY EXCLUDE_FROM_ALL YES)
