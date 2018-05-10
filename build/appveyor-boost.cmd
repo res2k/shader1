54079aa2434cbab2d141e8936f784f69c9186232
Frank Richter <frank.richter@gmail.com>
2018-05-10 18:24:44 +0200

    appveyor: Revert this mess

--- build/appveyor-boost.cmd
+++
@@ -1,8 +0,0 @@
-set BOOST_VERSION=%1
-if not exist C:\projects\deps\boost (
-  set BOOST_URL="http://sourceforge.net/projects/boost/files/boost/%BOOST_VERSION%/boost_%BOOST_VERSION:.=_%.7z"
-  appveyor DownloadFile %BOOST_URL% -FileName boost.7z
-  7z x boost.7z -oC:\projects\deps > nul
-  ren C:\projects\deps\boost_%BOOST_VERSION:.=_% boost
-  del boost.7z
-)
