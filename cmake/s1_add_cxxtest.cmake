08d43932cea85b28a7bb3421fcabba0af506ca84
Frank Richter <frank.richter@gmail.com>
2018-07-08 17:03:39 +0200

    cmake: Don't link object libraries through an intermediate '_dep' lib

--- cmake/s1_add_cxxtest.cmake
+++ cmake/s1_add_cxxtest.cmake
@@ -1,18 +1,26 @@
 # This is CXXTEST_ADD_TEST, modified to support generator expressions
 macro(S1_CXXTEST_ADD_TEST _cxxtest_testname _cxxtest_outfname)
-    set(_cxxtest_real_outfname ${CMAKE_CURRENT_BINARY_DIR}/${_cxxtest_outfname})
+  CMAKE_PARSE_ARGUMENTS(S1_CXXTEST_ADD_TEST
+    ""
+    ""
+    "TEST_SOURCES;LINK_SOURCES"
+    ${ARGN})
+  set(_cxxtest_real_outfname ${CMAKE_CURRENT_BINARY_DIR}/${_cxxtest_outfname})
 
-    add_custom_command(
-        OUTPUT  ${_cxxtest_real_outfname}
-        DEPENDS ${ARGN}
-        COMMAND ${CXXTEST_TESTGEN_INTERPRETER}
-        ${CXXTEST_TESTGEN_EXECUTABLE} ${CXXTEST_TESTGEN_ARGS} -o ${_cxxtest_real_outfname} ${ARGN}
-    )
+  add_custom_command(
+      OUTPUT  ${_cxxtest_real_outfname}
+      DEPENDS ${ARGN}
+      COMMAND ${CXXTEST_TESTGEN_INTERPRETER}
+      ${CXXTEST_TESTGEN_EXECUTABLE} ${CXXTEST_TESTGEN_ARGS} -o ${_cxxtest_real_outfname}
+        ${S1_CXXTEST_ADD_TEST_TEST_SOURCES} ${S1_CXXTEST_ADD_TEST_UNPARSED_ARGUMENTS}
+  )
 
-    set_source_files_properties(${_cxxtest_real_outfname} PROPERTIES GENERATED true)
-    add_executable(${_cxxtest_testname} ${_cxxtest_real_outfname} ${ARGN})
+  set_source_files_properties(${_cxxtest_real_outfname} PROPERTIES GENERATED true)
+  add_executable(${_cxxtest_testname} ${_cxxtest_real_outfname}
+                 ${S1_CXXTEST_ADD_TEST_TEST_SOURCES} ${S1_CXXTEST_ADD_TEST_UNPARSED_ARGUMENTS}
+                 ${S1_CXXTEST_ADD_TEST_LINK_SOURCES})
 
-    add_test(NAME ${_cxxtest_testname} COMMAND ${_cxxtest_testname})
+  add_test(NAME ${_cxxtest_testname} COMMAND ${_cxxtest_testname})
 endmacro()
 
 FUNCTION(s1_add_cxxtest TARGET)
@@ -23,12 +31,8 @@ FUNCTION(s1_add_cxxtest TARGET)
     ${ARGN})
   S1_GET_DEPENDENCIES(add_sources add_libs add_targets ${S1_ADD_CXXTEST_DEPENDS})
   S1_CXXTEST_ADD_TEST(${TARGET} ${TARGET}.cpp
-                      ${S1_ADD_CXXTEST_SOURCES})
-  IF(add_sources)
-    ADD_LIBRARY(${TARGET}_dep STATIC ${add_sources})
-    set_property(TARGET ${TARGET}_dep PROPERTY EXCLUDE_FROM_ALL YES)
-    target_link_libraries(${TARGET} ${TARGET}_dep)
-  ENDIF()
+                      TEST_SOURCES ${S1_ADD_CXXTEST_SOURCES}
+                      LINK_SOURCES ${add_sources})
   IF(add_libs)
     target_link_libraries(${TARGET} ${add_libs})
   ENDIF()
